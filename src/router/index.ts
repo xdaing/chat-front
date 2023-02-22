5cebb5961362241bf1bbb4d318b86bacb8f892b8
咩咩 <2380848997@qq.com>
2023-02-22 23:25:45 +0800

    feat:路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,84 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      redirect: '/message',
+      children: [
+        {
+          path: 'message',
+          component: () => import('@/views/Message/Index.vue'),
+          children: [
+            {
+              path: 'chat',
+              name: 'chat',
+              component: () => import('@/views/Message/Chat.vue')
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: '/contact',
+      meta: { requireLogin: true },
+      component: () => import('@/views/Contact/Index.vue'),
+      children: [
+        {
+          path: 'info',
+          component: () => import('@/views/Contact/ContactInfo.vue')
+        },
+        {
+          path: 'newContact',
+          component: () => import('@/views/Contact/NewContact.vue')
+        }
+      ]
+    },
+    {
+      path: '/search',
+      meta: { requireLogin: true },
+      component: () => import('@/views/Search/Index.vue'),
+      children: [
+        {
+          path: 'info',
+          component: () => import('@/views/Search/SearchInfo.vue')
+        }
+      ]
+    },
+    {
+      path: '/setting',
+      component: () => import('@/views/Setting/Index.vue'),
+      children: [
+        {
+          path: 'login',
+          component: () => import('@/views/Setting/Login.vue')
+        },
+        {
+          path: 'register',
+          component: () => import('@/views/Setting/Register.vue')
+        },
+        {
+          path: 'config',
+          component: () => import('@/views/Setting/Config.vue')
+        },
+        {
+          path: 'about',
+          component: () => import('@/views/Setting/About.vue')
+        },
+        {
+          path: 'user',
+          meta: { requireLogin: true },
+          component: () => import('@/views/Setting/User.vue')
+        }, {
+          path: 'reset',
+          component: () => import('@/views/Setting/Resetting.vue')
+        }
+      ]
+    }
+  ]
 })
 
+router.beforeEach(to => to.meta.requireLogin ? useUserStore().isLogin : true)
 export default router
